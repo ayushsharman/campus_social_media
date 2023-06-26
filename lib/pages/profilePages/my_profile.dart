441821f4828c6cedf614f090562f98373a5b94fc
Ayush Sharma <ashsharman123@gmail.com>
2023-06-26 08:46:21 +0530

    tagline fixing

--- lib/pages/profilePages/my_profile.dart
+++ lib/pages/profilePages/my_profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:social_media_campus_app/constant/appColos.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -8,47 +9,93 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Stack(
-              alignment: Alignment.topRight,
-              children: [
-                Container(
-                  height: 200, // Adjust the height as needed
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/Profile Banner.jpg'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 150.0, right: 8),
-                  child: Container(
-                    width: 120,
-                    height: 120,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Stack(
+                alignment: Alignment.topRight,
+                children: [
+                  //Banner
+                  Container(
+                    height: 200,
                     decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      gradient: LinearGradient(
-                        colors: [AppColor.blue, Colors.deepPurple],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(2.0),
-                      child: CircleAvatar(
-                        radius: 40,
-                        foregroundImage: AssetImage('assets/Profile.jpg'),
+                      image: DecorationImage(
+                        image: AssetImage('assets/Profile Banner.jpg'),
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //Name heading
+                      Padding(
+                        padding: const EdgeInsets.only(top: 200.0, left: 60),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Elizabeth Olsen",
+                              style: GoogleFonts.poppins(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Brooklyn NYK",
+                              style: GoogleFonts.poppins(
+                                color: Colors.grey,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      //Profile Picture
+                      Padding(
+                        padding: const EdgeInsets.only(top: 150.0, right: 8),
+                        child: Container(
+                          width: 120,
+                          height: 120,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            gradient: LinearGradient(
+                              colors: [AppColor.blue, Colors.deepPurple],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(2.0),
+                            child: CircleAvatar(
+                              radius: 40,
+                              foregroundImage: AssetImage('assets/Profile.jpg'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 15),
+
+              //Bio
+              Center(
+                child: Text(
+                  "Coder by Profession. Artist by Passion!",
+                  style: GoogleFonts.poppins(
+                    fontStyle: FontStyle.italic,
+                    color: AppColor.blue,
+                    fontSize: 16,
+                  ),
                 ),
-              ],
-            ),
-            // Add more profile information or content here
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
