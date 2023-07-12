5bcdcfe2115815d57b6e393f1526dd11add7bf58
Ayush Sharma <ashsharman123@gmail.com>
2023-07-12 16:25:23 +0530

    changed the ui a bit

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -21,28 +21,49 @@ class LoginPage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
-                const SizedBox(height: 60),
-                const Icon(
-                  Iconsax.login, // Add the login icon from iconsax
-                  size: 120,
-                  color: Colors.white,
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Login',
-                  style: GoogleFonts.poppins(
-                    fontSize: 60, // Increase the size of the login text
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                Container(
+                  width: double.infinity,
+                  height: 350,
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: AppColor.orange,
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 10,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  'A place to find your mate on campus!',
-                  style: TextStyle(
-                    fontSize: 20, // Increase the size of the login text
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.school, // Icon representing college
+                        size: 60,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Login',
+                        style: GoogleFonts.poppins(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'A place to find your mate on campus!',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 const SizedBox(height: 20),
@@ -75,12 +96,13 @@ class LoginPage extends StatelessWidget {
                       ),
                     );
                   },
-                  child: const Text(
+                  child: Text(
                     "Don't have an account? Register",
-                    style: TextStyle(
+                    style: GoogleFonts.poppins(
                       color: Colors.white,
                       fontSize: 16,
                       decoration: TextDecoration.underline,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
