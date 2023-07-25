dd1cad7aad55bb36ea5e3690f5f9d273bbf861c1
Ayush Sharma <ashsharman123@gmail.com>
2023-07-25 12:18:36 +0530

    no image

--- lib/pages/profilePages/my_profile.dart
+++ lib/pages/profilePages/my_profile.dart
@@ -1,9 +1,11 @@
-// ignore_for_file: library_private_types_in_public_api
+import 'dart:io';
 
 import 'package:Feeleeria/pages/profilePages/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../constant/app_colos.dart';
 import '../../widgets/home/posts/imagePost.dart';
@@ -21,6 +23,10 @@ class _ProfilePageState extends State<ProfilePage>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
+  final _imagePicker = ImagePicker();
+  XFile? _profileImage;
+  XFile? _bannerImage;
+
   @override
   void initState() {
     super.initState();
@@ -33,6 +39,77 @@ class _ProfilePageState extends State<ProfilePage>
     super.dispose();
   }
 
+  // Method to request permission for accessing the photo library
+  Future<bool> _requestPhotoLibraryPermission() async {
+    final status = await Permission.photos.request();
+    return status.isGranted;
+  }
+
+  // Method to pick an image from the photo library
+  Future<XFile?> _pickImage() async {
+    final bool isPermissionGranted = await _requestPhotoLibraryPermission();
+    if (!isPermissionGranted) return null;
+
+    final XFile? image =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+    return image;
+  }
+
+  // Method to handle banner picture selection
+  void _pickBannerPicture() async {
+    final XFile? imageFile = await _pickImage();
+    setState(() {
+      _bannerImage = imageFile;
+    });
+
+    if (imageFile != null) {
+      // Save the image to Firestore or use it as needed
+      // For example, you can upload the image to Firebase Storage and save the URL in Firestore.
+    }
+  }
+
+  // Method to show default text when no image is available
+  Widget _buildDefaultImageWidget(XFile? imageFile) {
+    return imageFile != null
+        ? Image.file(
+            File(imageFile.path),
+            width: 120,
+            height: 120,
+            fit: BoxFit.cover,
+          )
+        : Container(
+            width: 120,
+            height: 120,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                colors: [AppColor.blue, Colors.deepPurple],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const Center(
+              child: Text(
+                'No Image',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          );
+  }
+
+  // Method to handle profile picture selection
+  void _pickProfilePicture() async {
+    final XFile? imageFile = await _pickImage();
+    setState(() {
+      _profileImage = imageFile;
+    });
+
+    if (imageFile != null) {
+      // Save the image to Firestore or use it as needed
+      // For example, you can upload the image to Firebase Storage and save the URL in Firestore.
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,12 +120,26 @@ class _ProfilePageState extends State<ProfilePage>
             Stack(
               children: [
                 // Banner
-                Container(
-                  height: 200,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/Profile Banner.jpg'),
-                      fit: BoxFit.cover,
+                GestureDetector(
+                  onTap: _pickBannerPicture,
+                  child: Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      image: _bannerImage != null
+                          ? DecorationImage(
+                              image: FileImage(File(_bannerImage!.path)),
+                              fit: BoxFit.cover,
+                            )
+                          : _profileImage != null
+                              ? DecorationImage(
+                                  image: FileImage(File(_profileImage!.path)),
+                                  fit: BoxFit.cover,
+                                )
+                              : const DecorationImage(
+                                  image:
+                                      AssetImage('assets/Profile Banner.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
                     ),
                   ),
                 ),
@@ -100,22 +191,23 @@ class _ProfilePageState extends State<ProfilePage>
                     // Profile Picture
                     Padding(
                       padding: const EdgeInsets.only(top: 150.0, right: 8),
-                      child: Container(
-                        width: 120,
-                        height: 120,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          gradient: LinearGradient(
-                            colors: [AppColor.blue, Colors.deepPurple],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
+                      child: GestureDetector(
+                        onTap: _pickProfilePicture,
+                        child: Container(
+                          width: 120,
+                          height: 120,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            gradient: LinearGradient(
+                              colors: [AppColor.blue, Colors.deepPurple],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
                           ),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(2.0),
-                          child: CircleAvatar(
-                            radius: 40,
-                            foregroundImage: AssetImage('assets/Profile.jpg'),
+                          child: Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: _buildDefaultImageWidget(
+                                _profileImage), // Replace _profileImage with the profile image URL from Firestore
                           ),
                         ),
                       ),
@@ -178,7 +270,9 @@ class _ProfilePageState extends State<ProfilePage>
               labelColor: Colors.black,
               indicatorWeight: 3,
               labelStyle: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold, fontSize: 18),
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
               controller: _tabController,
               indicatorColor: AppColor.blue,
               tabs: const [
